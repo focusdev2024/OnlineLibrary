84bf81854f0e99bb6b30fad5203d2946a9495416
FocusDev <ownworkplace01@gmal.com>
2025-12-16 19:41:51 +0500

    read and buy page modified

--- lib/features/read_and_buy_page/presentation/read_and_buy_page.dart
+++ lib/features/read_and_buy_page/presentation/read_and_buy_page.dart
@@ -27,107 +27,127 @@ class _ReadAndBuyPageState extends State<ReadAndBuyPage> {
     return Scaffold(
       backgroundColor: Theme.of(context).canvasColor,
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: Stack(children: [
-            Positioned(
-              top: 10,
-              left: 5,
-              child: ClipOval(
+        child: Stack(children: [
+          Positioned(
+            child: Container(
+              height: 450,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(bookImageUrl),
+                  fit: BoxFit.cover,
+                ),
+              ),
+              child: SizedBox(
                 child: BackdropFilter(
-                  filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                  filter: ImageFilter.blur(sigmaX: 20, sigmaY: 20),
                   child: Container(
-                    width: 40,
-                    height: 40,
                     alignment: Alignment.center,
                     decoration: BoxDecoration(
                       color: Theme.of(context).shadowColor,
-                      shape: BoxShape.circle,
-                    ),
-                    child: IconButton(
-                      iconSize: 24,
-                      padding: EdgeInsets.zero,
-                      onPressed: () => Navigator.pop(context),
-                      icon: Icon(
-                        Icons.chevron_left_rounded,
-                        color: Theme.of(context).dividerColor,
-                      ),
                     ),
                   ),
                 ),
               ),
             ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ClipRRect(
-                  borderRadius: const BorderRadius.all(Radius.circular(30)),
-                  child: Image.asset(
-                    bookImageUrl,
-                    width: 230,
-                    height: 340,
-                    fit: BoxFit.cover,
+          ),
+          Positioned(
+            top: 10,
+            left: 10,
+            child: ClipOval(
+              child: BackdropFilter(
+                filter: ImageFilter.blur(sigmaX: 100, sigmaY: 100),
+                child: Container(
+                  width: 40,
+                  height: 40,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).shadowColor,
+                    shape: BoxShape.circle,
+                  ),
+                  child: IconButton(
+                    iconSize: 24,
+                    padding: EdgeInsets.zero,
+                    onPressed: () => Navigator.pop(context),
+                    icon: Icon(
+                      Icons.chevron_left_rounded,
+                      color: Theme.of(context).dividerColor,
+                    ),
                   ),
                 ),
-                const SizedBox(height: 20),
-                SizedBox(
-                  width: MediaQuery.of(context).size.width,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        bookTitle,
-                        maxLines: 2,
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                      const SizedBox(height: 5),
-                      Text(
-                        bookAuthor,
-                        maxLines: 1,
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                      const SizedBox(height: 10),
-                      const Text('About the author'),
-                      const Text(
-                        'About the author text',
-                        maxLines: 5,
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                      const SizedBox(height: 10),
-                      const Text(
-                        'Ovedview',
-                        maxLines: 3,
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                      const SizedBox(height: 10),
-                      const Text(
-                        'Overview text',
-                        maxLines: 2,
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                    ],
-                  ),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                const SizedBox(height: 10),
-              ],
-            ),
-            Positioned(
-              bottom: 20,
-              left: 0,
-              right: 0,
-              child: StyleButtonWidget(
-                buttonName: 'Download',
-                onTap: () {},
-                buttonColor: Theme.of(context).primaryColor,
-                buttonBorderColor: Theme.of(context).dividerColor,
               ),
             ),
-          ]),
-        ),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              const SizedBox(height: 60),
+              ClipRRect(
+                borderRadius: const BorderRadius.all(Radius.circular(24)),
+                child: Image.asset(
+                  bookImageUrl,
+                  width: 230,
+                  height: 340,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              const SizedBox(height: 20),
+              SizedBox(
+                width: MediaQuery.of(context).size.width,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      bookTitle,
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    const SizedBox(height: 5),
+                    Text(
+                      bookAuthor,
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    const SizedBox(height: 10),
+                    const Text('About the author'),
+                    const Text(
+                      'About the author text',
+                      maxLines: 5,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    const SizedBox(height: 10),
+                    const Text(
+                      'Ovedview',
+                      maxLines: 3,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    const SizedBox(height: 10),
+                    const Text(
+                      'Overview text',
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              const SizedBox(height: 10),
+            ],
+          ),
+          Positioned(
+            bottom: 20,
+            left: 10,
+            right: 10,
+            child: StyleButtonWidget(
+              buttonName: 'Download',
+              onTap: () {},
+              buttonColor: Theme.of(context).primaryColor,
+              buttonBorderColor: Theme.of(context).dividerColor,
+            ),
+          ),
+        ]),
       ),
     );
   }
